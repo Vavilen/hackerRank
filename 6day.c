b131fc8754ab78a5ce7927f7840243597a69f0b9
VavilenTatarskiy <levintsev@gmail.com>
2016-07-04 23:22:31 +0300

    done

--- 6day.c
+++ 6day.c
@@ -4,39 +4,30 @@
 #include <stdlib.h>
 
 int main() {
-    char *s = malloc(128);
-    int c = EOF;
     unsigned int num;
     unsigned int nnum;
     unsigned int current_size = 128;
     unsigned int ii;
     unsigned int i = 0;
     scanf("%d\n", &num);
-    for (nnum =0; nnum <= num; nnum++) {
+    for (nnum = 0; nnum < num; nnum++) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-            while( ( ( c = fgetc(stdin) ) != '\n' || i == 0) && c!= EOF) {
-                s[i++] = (char)c;
-                if (i == current_size) {
-                    current_size = i + 128;
-                    s = realloc(s, current_size);
-                }
-            }
-            char *odd = malloc(current_size / 2 + 1);
-            char *even = malloc(current_size / 2 + 1);
+            char string[10000];
+            scanf(" %[^\n]s",string);
+            char *odd = malloc(10000 / 2 + 1);
+            char *even = malloc(10000 / 2 + 1);
             unsigned oddCnt = 0;
             unsigned evenCnt = 0;
-            for (ii = 0; ii <= i; ii++) {
+            for (ii = 0; ii <= strlen(string) - 1; ii++) {
                 if (ii % 2 == 0) {
-                    even[evenCnt++] = (char)s[ii];
+                    even[evenCnt++] = (char)string[ii];
                 } else {
-                    odd[oddCnt++] = (char)s[ii];
+                    odd[oddCnt++] = (char)string[ii];
                 }
             }
             printf("%s %s\n", even, odd);
             i = 0;
             ii = 0;
-            c = EOF;
-            s = malloc(128);
     }
     return 0;
 }
