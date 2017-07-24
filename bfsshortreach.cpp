36e9949fcfc2923bf5a5c7822d5c5583f77c7fef
VavilenTatarskiy <levintsev@gmail.com>
2017-07-24 23:40:33 +0300

    +gridlandMetro

---
+++ bfsshortreach.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// идем по пути, пока не дойдем до старотового элемента. тогда записываем кол-во шагов до него от некоторого узла
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
+
