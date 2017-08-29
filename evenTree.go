a3d89aa920b45fa8fe202f5daed34439d2f15e12
VavilenTatarskiy <levintsev@gmail.com>
2017-08-30 00:13:46 +0300

    +evenTree

---
+++ evenTree.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// https://www.hackerrank.com/challenges/even-tree
+
+
+type Node struct {
+	parent int
+	childrenCount int
+}
+
+
+func main() {
+	var count int = 0
+	var n int // nodes number
+	var m int // edges number
+	fmt.Scanf("%d %d", &n, &m)
+	var nodes = make([]Node, n + 1)
+	var child, parent int
+	for i := 0; i < m; i++ {
+		fmt.Scanf("%d %d", &child, &parent)
+		nodes[child].parent = parent
+		nodes[parent].childrenCount++
+	}
+	for i := n; i > 1; i-- {
+		if nodes[i].childrenCount % 2 != 0 {
+			count++
+			nodes[nodes[i].parent].childrenCount--
+		}
+	}
+	fmt.Printf("%d\n", count)
+
+}
