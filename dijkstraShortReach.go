426e2a2c2151d8e473d624f264c4cf4cad39c27d
VavilenTatarskiy <levintsev@gmail.com>
2017-10-01 00:33:55 +0300

    dijkstraShortReach - Done!

--- dijkstraShortReach.go
+++ dijkstraShortReach.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"github.com/pkg/errors"
+	"errors"
+	"bufio"
+	"os"
+	"sync"
+	"strings"
 )
 
 // https://www.hackerrank.com/challenges/dijkstrashortreach
@@ -45,19 +48,14 @@ type TreeNode struct {
 }
 
 func (n *TreeNode) Insert(distance int, idx int) error {
-	if n == nil {
-		return errors.New("can`t insert into nil node")
-	}
 	switch {
-	case distance == n.Distance:
-		return nil
 	case distance < n.Distance:
 		if n.Left == nil {
 			n.Left = &TreeNode{Distance: distance, Idx: idx}
 		} else {
 			n.Left.Insert(distance, idx)
 		}
-	case distance > n.Distance:
+	case distance >= n.Distance:
 		if n.Right == nil {
 			n.Right = &TreeNode{Distance: distance, Idx: idx}
 		} else {
@@ -67,67 +65,62 @@ func (n *TreeNode) Insert(distance int, idx int) error {
 	return nil
 }
 
-func (n *TreeNode) ReplaceNode(parent, replacment *TreeNode) error {
+func (n *TreeNode) ReplaceNode(parent, replacement *TreeNode) error {
 	if n == nil {
 		return errors.New("can`t replace nil node")
 	}
 	if n == parent.Left {
-		parent.Left = replacment
+		parent.Left = replacement
 		return nil
 	}
-	parent.Right = replacment
+	parent.Right = replacement
 	return nil
 }
 
-func (n *TreeNode) Pop(root *TreeNode) *TreeNode {
-	if root.Left == nil {
-		return root
+func (t *Tree) Pop() *TreeNode {
+	if t.Root.Left == nil && t.Root.Right == nil {
+		var value *TreeNode = t.Root
+		t.Root = nil
+		return value
+	} else if t.Root.Left == nil {
+		var value *TreeNode = t.Root
+		t.Root = t.Root.Right
+		return value
 	}
-	if 
+	return t.PopMin(t.Root.Left, t.Root)
 }
 
-type Item struct {
-	value    int
-	priority int
-	index    int
+func (t *Tree) PopMin(current *TreeNode, parent *TreeNode) *TreeNode {
+	if current.Left == nil {
+		var result TreeNode
+		result = *current
+		if current.Right != nil {
+			*parent.Left = *current.Right
+		} else {
+			parent.Left = nil
+		}
+		return &result
+	}
+	return t.PopMin(current.Left, current)
 }
 
-type PriorityQueue []*Item
 
-// Len - get the length of the heap
-func (pq PriorityQueue) Len() int { return len(pq) }
 
-// Less - determine which is more priority than another
-func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].priority > pq[j].priority
+func Insert(heap *Tree, distance int, idx int) error {
+	if heap.Root == nil {
+		heap.Root = &TreeNode{
+			Distance: distance,
+			Idx:      idx,
+			Left:     nil,
+			Right:    nil,
+		}
+		return nil
+	}
+	err := heap.Root.Insert(distance, idx)
+	return err
 }
 
-// Swap - implementation of swap for the heap interface
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-// Push - implementation of push for the heap interface
-func (pq *PriorityQueue) Push(x *Item) {
-	n := len(*pq)
-	item := x
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-// Pop - implementation of pop for heap interface
-func (pq *PriorityQueue) Pop() *Item {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	item.index = -1 // for safety
-	*pq = old[0: n-1]
-	return item
-}
-
-func process() {
+func process(scanner *bufio.Scanner, nodes *[]*Node, wg *sync.WaitGroup) {
 	var n int // nodes count
 	var m int // edges count
 
@@ -137,76 +130,107 @@ func process() {
 
 	var s int // start node
 
-	fmt.Scanf("%d %d", &n, &m)
-	var nodes = make([]*Node, n+1)
+	if !scanner.Scan() {
+		return
+	}
+	fmt.Sscanf(scanner.Text(), "%d %d", &n, &m)
+	nodesRaw := make([]string, m)
 	for i := 0; i < m; i++ {
-		fmt.Scanf("%d %d %d", &x, &y, &r)
-		if nodes[x] == nil {
-			nodes[x] = &Node{
-				id:        x,
-				edges:     make(map[int]int, 0),
-				weight:    DEFAULT_WEIGHT,
-				processed: false,
+		scanner.Scan()
+		nodesRaw[i] = scanner.Text()
+	}
+	if !scanner.Scan() {
+		return
+	}
+	*nodes = make([]*Node, n + 1)
+	fmt.Sscanf(scanner.Text(), "%d", &s)
+	go func(nodesRaw []string, n, m, s int, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for i := 0; i < m; i++ {
+			splitted := strings.Split(nodesRaw[i], " ")
+			x = toInt([]byte(splitted[0]))
+			y = toInt([]byte(splitted[1]))
+			r = toInt([]byte(splitted[2]))
+			if (*nodes)[x] == nil {
+				(*nodes)[x] = &Node{
+					id:        x,
+					edges:     make(map[int]int, 0),
+					weight:    DEFAULT_WEIGHT,
+					processed: false,
+				}
 			}
-		}
-		if nodes[y] == nil {
-			nodes[y] = &Node{
-				id:        y,
-				edges:     make(map[int]int, 0),
-				weight:    DEFAULT_WEIGHT,
-				processed: false,
+			if (*nodes)[y] == nil {
+				(*nodes)[y] = &Node{
+					id:        y,
+					edges:     make(map[int]int, 0),
+					weight:    DEFAULT_WEIGHT,
+					processed: false,
+				}
 			}
+
+			(*nodes)[x].addEdge(y, r)
+			(*nodes)[y].addEdge(x, r)
 		}
 
-		nodes[x].addEdge(y, r)
-		nodes[y].addEdge(x, r)
-	}
-	fmt.Scanf("%d", &s)
-	nodes[s].weight = 0
-	var heap = PriorityQueue{}
-	heap.Push(
-		&Item{
-			value:    s,
-			priority: 0,
-		},
-	)
-	for heap.Len() > 0 {
-		var current = heap.Pop()
-		if nodes[current.value].processed {
-			continue
+
+		(*nodes)[s].weight = 0
+		var heap = &Tree{}
+		heap.Root = &TreeNode{
+			Distance: 0,
+			Idx:      s,
+			Left:     nil,
+			Right:    nil,
 		}
-		nodes[current.value].processed = true
-		for stop, distance := range nodes[current.value].edges {
-			if !nodes[stop].processed && (nodes[stop].weight == DEFAULT_WEIGHT || nodes[stop].weight > nodes[current.value].weight+distance) {
-				nodes[stop].weight = nodes[current.value].weight + distance
-				heap.Push(
-					&Item{
-						value:    stop,
-						priority: nodes[stop].weight,
-					},
-				)
+		for heap.Root != nil {
+			var current = heap.Pop()
+			if (*nodes)[current.Idx].processed {
+				continue
+			}
+			(*nodes)[current.Idx].processed = true
+			for stop, distance := range (*nodes)[current.Idx].edges {
+				if !(*nodes)[stop].processed && ((*nodes)[stop].weight == DEFAULT_WEIGHT || (*nodes)[stop].weight > (*nodes)[current.Idx].weight+distance) {
+					(*nodes)[stop].weight = (*nodes)[current.Idx].weight + distance
+					Insert(heap,(*nodes)[stop].weight, stop)
+				}
 			}
 		}
-		sort.Sort(heap)
-	}
-
-	for i := 1; i <= n; i++ {
-		if i == s {
-			continue
-		}
-		fmt.Printf("%d", nodes[i].weight)
-		if i < n {
-			fmt.Print(" ")
-		}
-	}
-	fmt.Print("\n")
+	}(nodesRaw, n, m, s, wg)
 }
 
 func main() {
 	var t int // tests count
-	fmt.Scanf("%d", &t)
+	var wg sync.WaitGroup
+	scanner := bufio.NewScanner(os.Stdin)
+	//buf, err := ioutil.ReadAll(reader)
 
-	for i := 0; i < t; i++ {
-		process()
+	if !scanner.Scan() {
+		return
 	}
+	fmt.Sscanf(scanner.Text(), "%d", &t)
+	var nodesnodes = make([][]*Node, t)
+	for i := 0; i < t; i++ {
+		wg.Add(1)
+		process(scanner, &nodesnodes[i], &wg)
+	}
+	wg.Wait()
+	for ii := 0; ii < t; ii++ {
+		for j := 1; j <= len(nodesnodes[ii]) - 1; j++ {
+			if nodesnodes[ii][j].weight == 0 {
+				continue
+			}
+			fmt.Printf("%d", nodesnodes[ii][j].weight)
+			if j < len(nodesnodes[ii]) {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Print("\n")
+	}
+
+}
+
+func toInt(buf []byte) (n int) {
+	for _, v := range buf {
+		n = n*10 + int(v-'0')
+	}
+	return
 }
